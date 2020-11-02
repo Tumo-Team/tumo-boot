abd983a5f90b59817c4463a0c2d90ed108fbf1f7
tycoding <tycoding@sina.com>
2020-11-02 09:24:28 +0800

    fix

--- tumo-boot-api/src/test/java/PasswordGenerateTest.java
+++ tumo-boot-api/src/test/java/PasswordGenerateTest.java
@@ -1,5 +1,5 @@
 import cn.tycoding.boot.TumoBootApp;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
