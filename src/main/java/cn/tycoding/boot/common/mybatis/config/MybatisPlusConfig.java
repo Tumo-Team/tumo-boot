f0b87a848e9a64fb06ba3b9e58e6000083f9f2d0
TyCoding <tycoding@sina.com>
2021-06-30 11:24:35 +0800

    更新依赖版本

--- src/main/java/cn/tycoding/boot/common/mybatis/config/MybatisPlusConfig.java
+++ src/main/java/cn/tycoding/boot/common/mybatis/config/MybatisPlusConfig.java
@@ -1,7 +1,9 @@
 package cn.tycoding.boot.common.mybatis.config;
 
 import cn.tycoding.boot.common.mybatis.intercept.SqlLogInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,8 +21,10 @@ public class MybatisPlusConfig {
      * 分页插件
      */
     @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        return new PaginationInterceptor();
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
     }
 
     /**
